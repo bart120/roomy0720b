d0c1477aafc4826c4205059458f98a9ef348c04c
Vincent <vleclerc@inow.fr>
2020-06-29 15:50:55 +0200

    Add RoomsModule and FooterComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { RoomsModule } from './pages/rooms/rooms.module';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { MaterialModule } from './material.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    RoomsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
