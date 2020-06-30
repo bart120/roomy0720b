7e54c12b4225b3c261163e990fadc2708e1b2b1f
Vincent <vleclerc@inow.fr>
2020-06-30 14:01:47 +0200

    ngIf

--- src/app/pages/authentication/login/login.component.ts
+++ src/app/pages/authentication/login/login.component.ts
@@ -11,6 +11,8 @@ export class LoginComponent implements OnInit {
   @ViewChild('password', { static: false })
   private passwd: ElementRef;
 
+  user: any = null;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -19,6 +21,11 @@ export class LoginComponent implements OnInit {
   onLogin(mail: string, ev: any) {
     console.log("test:", mail, this.passwd.nativeElement.value);
     console.log(ev);
+    this.user = { login: mail, name: mail };
+  }
+
+  onLogout() {
+    this.user = null;
   }
 
 }
