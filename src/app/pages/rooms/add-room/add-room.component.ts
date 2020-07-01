1bd6bce29bdac0901229922365e4b520bb95b995
Vincent <vleclerc@inow.fr>
2020-07-01 11:53:56 +0200

    FormGroup

--- src/app/pages/rooms/add-room/add-room.component.ts
+++ src/app/pages/rooms/add-room/add-room.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { RoomService } from '../services/room.service';
+import { RoomModel } from 'src/app/models/room.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-add-room',
@@ -8,9 +12,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddRoomComponent implements OnInit {
 
-  constructor() { }
+  formRoom = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    price: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]*$/)]),
+    seatCount: new FormControl('10'),
+    image: new FormControl('', [Validators.required])
+  });
+
+  pictures: Array<any> = [
+    { file: 'Pegase.jpg', name: 'Pegase' },
+    { file: 'Calliope.jpg', name: 'Calliope' },
+    { file: 'Thalie.jpg', name: 'Thalie' }
+  ];
+
+  constructor(private serv: RoomService, private snack: MatSnackBar) { }
 
   ngOnInit(): void {
+  }
+
+  submit() {
+    //console.log(this.formRoom);
+    this.serv.insert(this.formRoom.value as RoomModel).subscribe(
+      data => {
+        this.snack.open(`La salle ${data.name} est enregistrée avec l'id ${data.id}`, 'OK', { duration: 4000, verticalPosition: 'top' });
+      },
+      err => {
+        this.snack.open(`Erreur lors de l'enregistrement: ${err.message}`, 'OK', { duration: 4000, verticalPosition: 'top' });
+      }
+    );
   }
 
 }
--- src/app/pages/rooms/rooms.module.ts
+++ src/app/pages/rooms/rooms.module.ts
@@ -6,6 +6,7 @@ import { AddRoomComponent } from './add-room/add-room.component';
 import { ListRoomComponent } from './list-room/list-room.component';
 import { DetailRoomComponent } from './detail-room/detail-room.component';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     RouterModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ],
   exports: [
     FooterComponent
